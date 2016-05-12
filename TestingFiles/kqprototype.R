3462e59383d9ba957b0d51204d3fb3938f900a87
Dave Ho <daveho8888@gmail.com>
2016-05-11 22:32:27 -0700

    rawData extracted from JSON String

--- TestingFiles/kqprototype.R
+++ TestingFiles/kqprototype.R
@@ -3,3 +3,4 @@ api_key <- "ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
 domain <- "https://apps.zetaphase.io/kq/admin/api.php?apikey="
 url <- paste(domain, api_key, sep="")
 document <- fromJSON(txt=url)
+data <- document[["rawData"]]
