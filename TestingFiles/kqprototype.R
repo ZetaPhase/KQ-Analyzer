78834e621d4551a4f8ae1926fc4959484fa4601e
Dave Ho <daveho8888@gmail.com>
2016-05-11 22:22:05 -0700

    variable reorientation
    cleaner code

--- TestingFiles/kqprototype.R
+++ TestingFiles/kqprototype.R
@@ -1,3 +1,5 @@
 library(jsonlite)
-url <- "https://apps.zetaphase.io/kq/admin/api.php?apikey=ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
+api_key <- "ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
+domain <- "https://apps.zetaphase.io/kq/admin/api.php?apikey="
+url <- paste(domain, api_key, sep="")
 document <- fromJSON(txt=url)
