14195e7bd504a012f579262b94fa05ee0390d2a1
Dave Ho <daveho8888@gmail.com>
2016-05-12 20:33:23 -0700

    hide api key

--- KQ-Analyzer/server.R
+++ KQ-Analyzer/server.R
@@ -12,7 +12,6 @@ library(jsonlite)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  api_key <- "ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
   domain <- "https://apps.zetaphase.io/kq/admin/api.php?apikey="
   url <- paste(domain, api_key, sep="")
   document <- fromJSON(txt=url)
--- TestingFiles/kqprototype.R
+++ TestingFiles/kqprototype.R
@@ -1,5 +1,4 @@
 library(jsonlite)
-api_key <- "ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
 domain <- "https://apps.zetaphase.io/kq/admin/api.php?apikey="
 url <- paste(domain, api_key, sep="")
 document <- fromJSON(txt=url)
