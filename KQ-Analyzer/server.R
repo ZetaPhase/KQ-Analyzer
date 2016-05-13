561441c4da3bbb6718634cc9cfc68f8baed659b5
Dave Ho <daveho8888@gmail.com>
2016-05-12 17:48:10 -0700

    generate data table from id section in data

--- KQ-Analyzer/server.R
+++ KQ-Analyzer/server.R
@@ -12,10 +12,17 @@ library(jsonlite)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
+  api_key <- "ab8fEq6hD2tA39BwHeUU483cuNDm4taDNhK9Wtfc"
+  domain <- "https://apps.zetaphase.io/kq/admin/api.php?apikey="
+  url <- paste(domain, api_key, sep="")
+  document <- fromJSON(txt=url)
+  data <- document[["rawData"]]
   
   output$idPlot <- renderPlot({
-    
+    # generate data table from id section in data
+    idData <- table(data[["id"]])
   })
+  
   output$distPlot <- renderPlot({
     
     # generate bins based on input$bins from ui.R
